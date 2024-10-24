c66b86434a9a56771aa1e42437202c003da801bf
oladeeayo <oladayo225@gmail.com>
2024-10-24 22:30:47 +0100

    Updated build and source files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.css';  // Optional, if you have global styles
+import QuestionCards from './components/QuestionCards';  // Import your main component
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QuestionCards />
   </React.StrictMode>
 );
